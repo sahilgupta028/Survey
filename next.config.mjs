044a9b3c55916af82e51f88fc24e3f629498d0db
sahilgupta028 <149893310+sahilgupta028@users.noreply.github.com>
2025-02-22 15:24:37 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    serverRuntimeConfig: {
+        host: '0.0.0.0',
+        port: 3000,
+      }
+};
 
 export default nextConfig;
