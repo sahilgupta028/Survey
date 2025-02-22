1f113fe0fce849a4ed2321b5e211e3258befc290
sahilgupta028 <149893310+sahilgupta028@users.noreply.github.com>
2025-02-22 16:38:05 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    optimizeFonts: true, // Optimizes font loading
-  },
 };
 
 export default nextConfig;
