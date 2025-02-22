c00d699eeb94c64e8f4357afd8c39ef87a4f4606
sahilgupta028 <149893310+sahilgupta028@users.noreply.github.com>
2025-02-22 15:29:42 +0530

    changed

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -6,7 +6,7 @@ const connectMongo = async () => {
   if (mongoose.connection.readyState >= 1) return;
 
   try {
-    await mongoose.connect(process.env.NEXT_APP_MONGODB_URL, {
+    await mongoose.connect(process.env.NODE_ENV_MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
